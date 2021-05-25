eaabfb630e034b485fa51fadf0f43af57976138a
Matheus Felipe dos Anjos <anjos97.matheus@gmail.com>
2021-05-25 01:41:41 -0300

    Fact table implemented - Songplays table added;

--- sql_queries.py
+++ sql_queries.py
@@ -1,6 +1,6 @@
 # DROP TABLES
 
-songplay_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
 user_table_drop = "DROP TABLE IF EXISTS users"
 song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
@@ -9,15 +9,25 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 # CREATE TABLES
 
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays (
+        timestamp timestamp,
+        user_id VARCHAR,
+        level VARCHAR,
+        song_id VARCHAR,
+        artist_id VARCHAR,
+        session_id VARCHAR PRIMARY KEY,
+        location VARCHAR,
+        user_agent VARCHAR
+    )
 """)
 
 user_table_create = ("""
     CREATE TABLE IF NOT EXISTS users (
         user_id VARCHAR PRIMARY KEY,
-        first_name varchar,
-        last_name varchar,
+        first_name VARCHAR,
+        last_name VARCHAR,
         gender char,
-        level varchar
+        level VARCHAR
     )
 """)
 
@@ -56,11 +66,14 @@ time_table_create = ("""
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT INTO songplays( timestamp, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    VALUES ( %s, %s, %s, %s, %s, %s, %s, %s )
+    ON CONFLICT DO NOTHING;
 """)
 
 user_table_insert = ("""
     INSERT INTO users ( user_id, first_name, last_name, gender, level )
-    VALUES (  %s, %s, %s, %s, %s )
+    VALUES ( %s, %s, %s, %s, %s )
     ON CONFLICT DO NOTHING;
 """)
 
@@ -86,6 +99,18 @@ time_table_insert = ("""
 # FIND SONGS
 
 song_select = ("""
+    SELECT 
+        songs.song_id,
+        artists.artist_id
+    FROM 
+        songs
+    JOIN
+        artists
+        ON artists.artist_id = songs.artist_id
+    WHERE
+        artists.artist_name = %s
+        AND songs.title = %s
+        AND songs.duration = %s
 """)
 
 # QUERY LISTS
